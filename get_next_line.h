8d87bc1227ced8d80415d6cf650899f12912ce81
Kurt Butor-Blamont <kbutor-b@1G6.42angouleme.fr>
2023-11-16 06:34:09 +0100

    16/11/23-F

---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbutor-b <kbutor-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 00:44:38 by kbutor-b          #+#    #+#             */
+/*   Updated: 2023/11/16 03:00:32 by kbutor-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h> //JUST FOR MAIN//
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+t_list	*ft_lstlast(t_list *list);
+int		line_len(t_list *list);
+int		new_line(t_list *list);
+size_t	ft_strlen(const char *str);
+void	lstclear(t_list **list);
+
+#endif
